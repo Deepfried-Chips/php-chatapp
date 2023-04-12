8c248094d426e440365389cfd246eb4e251b007b
Deepfried Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2023-04-12 13:46:20 +0300

    set up inertia with svelte

---
+++ resources/js/Pages/home.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { Link } from '@inertiajs/svelte'
+
+    onMount(() => {
+        console.log("wawa");
+    });
+</script>
+
+<svelte:head>
+    <title>Wawa</title>
+</svelte:head>
+
+<h1>
+    Wawa
+</h1>
+
+<Link href="/">Home</Link>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+        svelte(),
+        laravel.default({
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
