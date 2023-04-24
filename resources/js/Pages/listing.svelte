f9130a512205ae055317a93b30f2442c2ca565f7
Deepfried Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2023-04-24 13:27:29 +0300

    minor style changes, add margin, add back to listings link

--- resources/js/Pages/listing.svelte
+++ resources/js/Pages/listing.svelte
@@ -6,7 +6,11 @@
     <title>{listing.title} | Generic ChatApp</title>
 </svelte:head>
 
+<nav>
+    <a href="/listings" class="text-sky-800 hover:underline">Back to listings</a>
+</nav>
+
 <div>
-    <h1>{listing.title}</h1>
-    <p>{listing.description}</p>
+    <h1 class="font-bold text-3xl mt-2 mb-2">{listing.title}</h1>
+    <p class="text-xl">{listing.description}</p>
 </div>
--- resources/js/Pages/listings.svelte
+++ resources/js/Pages/listings.svelte
@@ -5,13 +5,13 @@
 </script>
 
 <svelte:head>
-    <title>{listings.length} Listings | Generic ChatApp</title>
+    <title>{listings.length} {listings.length ? "Listing" : "Listings"} | Generic ChatApp</title>
 </svelte:head>
 
 <ul>
 {#each listings as listing}
     <li>
-        <h1 class="text-3xl font-bold text-cyan-500 underline"><Link href="/listing/{listing.id}">{listing.title}</Link></h1>
+        <h1 class="text-3xl font-bold text-cyan-500 hover:underline"><Link href="/listing/{listing.id}">{listing.title}</Link></h1>
         <p class="text-xl">{listing.description}</p>
     </li>
 {/each}
