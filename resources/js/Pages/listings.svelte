ce1c28ceddb0241122533c10bb88693c1e6c479d
Deepfried Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2023-04-25 13:53:21 +0300

    more style changes

--- resources/js/Pages/listings.svelte
+++ resources/js/Pages/listings.svelte
@@ -11,7 +11,7 @@
 <ul>
 {#each listings as listing}
     <li>
-        <h1 class="text-3xl font-bold text-cyan-500 hover:underline"><Link href="/listing/{listing.id}">{listing.title}</Link></h1>
+        <h1 class="text-3xl font-bold text-sky-800 hover:underline"><Link href="/listing/{listing.id}">{listing.title}</Link></h1>
         <p class="text-xl">{listing.description}</p>
     </li>
 {/each}
